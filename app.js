f50d2588667ef1c6b59f9c926cb8acbf100c93e5
melah cer <melahcer@melahs-MacBook-Pro.local>
2025-04-22 17:59:51 +0100

    feat: configured Mustache, sessions, and flash in app.js added static images to public/images dir

--- app.js
+++ app.js
@@ -2,17 +2,46 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+// Importing required packages
+const mustacheExpress = require("mustache-express");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// Serve static imges from public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// creating simple route to test
+// Set up Mustache
+app.engine("mustache", mustacheExpress());
+// Tell Express to use Mustache to render
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+
+// Session config
+app.use(
+    session({
+      secret: "dance-secret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  // Flash messages
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_message = req.flash("success_message");
+  res.locals.error_message = req.flash("error_message");
+  next();
+});
+
+// Creating simple route to test
 app.get("/", (req, res) => {
   res.send("Hello Mr Robot 🤖 Dance 🕺💃 Dance 🕺💃 Dance 🕺💃");
 });
 
+// Starts the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
